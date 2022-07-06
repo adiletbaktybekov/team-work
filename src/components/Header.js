b21ad3ea3de3523bae534c3cda201602a6c0d7b3
kgadiletofficial <kgadiletofficial@gmail.com>
2022-07-06 15:45:32 +0600

    initial

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <section id="header">
+            <div className="container">
+                <div className="header">
+                        <h1>SONUN DEVS</h1>
+                    <div className="header-nav">v
+                        <p>adilet</p>
+                        <p>kunsuluu</p>
+                        <p>janybek</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Header;
